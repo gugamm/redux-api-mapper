a86b63c7f37122ca9aeca4eca1f0037fbdc3eceb
gugamm <gugamm@live.com>
2017-03-02 23:09:06 -0800

    Fix build error

--- src/api-connect.js
+++ src/api-connect.js
@@ -5,7 +5,7 @@ function apiConnect(apiToProps) {
     class ApiConnected extends Component {
       constructor(props, context) {
         super(props, context);
-        this.apiProps = apiToProps(context.api, {...props});
+        this.apiProps = apiToProps(context.api, Object.assign({}, props));
       }
 
       render() {
