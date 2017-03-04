6f8446aeda490ddde0814f68bc03edabf95e8fbe
gugamm <gugamm@live.com>
2017-03-04 04:21:21 -0800

    Fix export typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export {default as apiConnect} from './api-connect';
 export {default as ApiProvider} from './ApiProvider';
 export {default as FetchStates} from './fetch-states';
-export {default as HttpMethod} from './http-methods';
+export {default as HttpMethods} from './http-methods';
 export { createMapper, addResourceToMapper, addEndPointToMapper} from './mapper';
 export {default as stateToAction} from './state-to-action';
 export {default as createMockMapper} from './mock';
