1a25941bb7205eee94537a593a99798e0dec4753
gugamm <gugamm@live.com>
2017-04-05 16:49:55 -0700

    remove FetchHttpLayer export

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ export {FetchStates,createMapper,HttpMethods} from './mapper';
 export {createApiReducer} from './reducer';
 export {stateToAction} from './helpers';
 export {ApiProvider, apiConnect} from './provider';
-export {FetchHttpLayer} from './http-layers';
