667d69143a476ff17db46a947338da63ff7da96c
gugamm <gugamm@live.com>
2017-08-17 15:01:33 -0300

    bump to version 4.1.0

--- src/reducer/api-reducer.js
+++ src/reducer/api-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import buildActionBuilder from './action-builder';
-import buildReducerBuilder from './default-reducer';
+import defReducerBuilder from './default-reducer';
 
 /**
  * Generates a combined reducer for all api end-points based on the configuration provided
@@ -22,26 +22,26 @@ function createResourceReducer(config, resource) {
     const actionBuilder = buildActionBuilder(resource.name, endPoint.name, config.name);
     const name = endPoint.reducerName || endPoint.name;
 
+    let reducerData;
+
+    if (endPoint.reducerData !== undefined)
+      reducerData = endPoint.reducerData;
+    else if (resource.reducerData !== undefined)
+      reducerData = resource.reducerData;
+    else if (config.reducerData !== undefined)
+      reducerData = config.reducerData;
+    else
+      reducerData = undefined;
+
     if (endPoint.reducerBuilder)
-      acc[name] = endPoint.reducerBuilder(actionBuilder);
+      acc[name] = endPoint.reducerBuilder(actionBuilder, reducerData);
     else if (resource.reducerBuilder)
-      acc[name] = resource.reducerBuilder(actionBuilder);
+      acc[name] = resource.reducerBuilder(actionBuilder, reducerData);
     else if (config.reducerBuilder)
-      acc[name] = config.reducerBuilder(actionBuilder);
-    else {
-      let reducerData;
-
-      if (endPoint.reducerData !== undefined)
-        reducerData = endPoint.reducerData;
-      else if (resource.reducerData !== undefined)
-        reducerData = resource.reducerData;
-      else if (config.reducerData !== undefined)
-        reducerData = config.reducerData;
-      else
-        reducerData = null;
-
-      acc[name] = buildReducerBuilder(reducerData)(actionBuilder);
-    }
+      acc[name] = config.reducerBuilder(actionBuilder, reducerData);
+    else 
+      acc[name] = defReducerBuilder(actionBuilder, reducerData);
+    
     return acc;
   }, {});
 
--- src/reducer/default-reducer.js
+++ src/reducer/default-reducer.js
@@ -1,34 +1,32 @@
 import { FetchStates } from '../mapper';
 
-function buildReducerBuilder(initialData) {
+function defReducerBuilder(actionBuilder, reducerData) {
   const defState = {
     isFetching    : false,
     isError       : false,
     isSuccess     : false,
     isCancelled   : false,
     cancelledData : null,
-    data          : initialData || null,
+    data          : reducerData || null,
     errorData     : null
   };
 
-  return function defReducerBuilder(actionBuilder) {
-    const FETCH_STARTED   = actionBuilder(FetchStates.FETCH_STARTED);
-    const FETCH_COMPLETED = actionBuilder(FetchStates.FETCH_COMPLETED);
-    const FETCH_ERROR     = actionBuilder(FetchStates.FETCH_ERROR);
-    const FETCH_CANCELLED = actionBuilder(FetchStates.FETCH_CANCELLED);
-    const CLEAR_STATE     = actionBuilder("CLEAR_STATE");
+  const FETCH_STARTED   = actionBuilder(FetchStates.FETCH_STARTED);
+  const FETCH_COMPLETED = actionBuilder(FetchStates.FETCH_COMPLETED);
+  const FETCH_ERROR     = actionBuilder(FetchStates.FETCH_ERROR);
+  const FETCH_CANCELLED = actionBuilder(FetchStates.FETCH_CANCELLED);
+  const CLEAR_STATE     = actionBuilder("CLEAR_STATE");
 
-    return function reducer(prevState = defState, action) {
-      switch (action.type) {
-        case FETCH_STARTED   : return {...defState, isFetching: true};
-        case FETCH_COMPLETED : return {...defState, isSuccess : true, data : action.payload.data};
-        case FETCH_CANCELLED : return {...defState, isCancelled : true, cancelledData : action.payload.data};
-        case FETCH_ERROR     : return {...defState, isError : true, errorData : action.payload.data};
-        case CLEAR_STATE     : return defState;
-      }
-      return prevState;
-    };
+  return function reducer(prevState = defState, action) {
+    switch (action.type) {
+      case FETCH_STARTED   : return {...defState, isFetching: true};
+      case FETCH_COMPLETED : return {...defState, isSuccess : true, data : action.payload.data};
+      case FETCH_CANCELLED : return {...defState, isCancelled : true, cancelledData : action.payload.data};
+      case FETCH_ERROR     : return {...defState, isError : true, errorData : action.payload.data};
+      case CLEAR_STATE     : return defState;
+    }
+    return prevState;
   };
-}
+};
 
-export default buildReducerBuilder;
+export default defReducerBuilder;
